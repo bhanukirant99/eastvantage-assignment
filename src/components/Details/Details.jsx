c19938eef43510c9bd5283ed34579424778a9ee9
Bhanu Kiran <bhanukirant99@gmail.com>
2022-06-22 23:21:33 +0530

    feat: fetching random user from randomUser API

---
+++ src/components/Details/Details.jsx
@@ -0,0 +1,63 @@
+import React, { useState, useEffect } from "react";
+import { BASE_URL } from "../../constants";
+import axios from "axios";
+import "./Details.css";
+
+
+const Details = () => {
+  const [user, setUser] = useState({});
+  const [userName, setUserName] = useState();
+  const [userEmail, setUserEmail] = useState();
+  
+
+  useEffect(() => {
+    getUserFromAPI();
+    getUserFromLocalStorage()
+  });
+
+  const getUserFromAPI = async () => {
+    try {
+      const response = await axios.get(BASE_URL);
+
+      const responseUser = response.data.results[0];
+
+      localStorage.setItem("userEmail", responseUser.email);
+      localStorage.setItem("userName", responseUser.name["title"] + " " + responseUser.name["first"] + " " + responseUser.name["last"]);
+      // getUserFromLocalStorage()
+      console.log(responseUser);
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  const getUserFromLocalStorage = () => {
+    const userEmail = localStorage.getItem("userEmail");
+    const userName = localStorage.getItem("userName");
+
+    setUserEmail(userEmail)
+    setUserName(userName)  
+  }
+
+
+  const refresh = () => {
+    // re-renders the component
+    getUserFromLocalStorage();
+  };
+
+  return (
+    <div className="App">
+      <h2>Hi There, This is an app to fetch random users after every 2 seconds</h2>
+      <h2>Click on the Refresh button to get a new user</h2>  
+      <div>
+        <button id="refresh__button" onClick={refresh}>Refresh</button>
+      </div>
+
+      <div className="user">
+        <label id="user__name">Name: {userName}</label>
+        <label id="user__email">Email: {userEmail}</label>
+      </div>
+    </div>
+  );
+}
+
+export default Details;
